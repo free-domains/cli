59bea82f85f782ccc21010c957a0fddb880e1c8f
William Harrison <william@williamdavidharrison.com.au>
2023-02-03 18:02:06 +0800

    add second message to not logged in

--- src/functions/account.js
+++ src/functions/account.js
@@ -3,7 +3,11 @@ const Conf = require("conf");
 const account = new Conf();
 
 module.exports = function () {
-    if(!account.has("username")) return console.log("You are not logged in!");
+    if(!account.has("username")) {
+        console.log("You are not logged in!");
+        console.log("To log in, run the command: `domains login`");
+        return;
+    }
 
     console.log(`Username: ${account.get("username")}\nEmail: ${account.get("email")}\nToken: ${account.get("token")}`);
 }
