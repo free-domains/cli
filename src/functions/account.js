c47bf1e636c900fbe9dcc7874f07b0e2a71d0674
William Harrison <william@williamdavidharrison.com.au>
2023-02-04 15:41:54 +0800

    fix: same function name

--- src/functions/account.js
+++ src/functions/account.js
@@ -1,13 +1,17 @@
 const Conf = require("conf");
 
-const account = new Conf();
+const userAccount = new Conf();
 
 module.exports = function account() {
-    if(!account.has("username")) {
+    const username = userAccount.get("username");
+    const email = userAccount.get("email");
+    const token = userAccount.get("token");
+
+    if(!username) {
         console.log("You are not logged in!");
         console.log("To log in, run the command: `domains login`");
         return;
     }
 
-    console.log(`Username: ${account.get("username")}\nEmail: ${account.get("email")}\nToken: ${account.get("token")}`);
+    console.log(`Username: ${username}\nEmail: ${email}\nToken: ${}`);
 }
